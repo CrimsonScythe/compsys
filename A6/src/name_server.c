1a40fd6912b90dbf964b84f75c96d00985f251e4
CrimsonScythe <crimsonscythestudos@gmail.com>
2019-12-31 12:10:06 +0100

    a6 submission

--- A6/src/name_server.c
+++ A6/src/name_server.c
@@ -7,27 +7,27 @@ enum status {NONE, LOGIN_S, LOGIN_S_END, LOOKUP_S, LOOKUP_S_END ,LOGOUT_S};
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 char listen_port[PORT_LEN];
+
 int  listen_socket     = -1;       // listen socket. initialized to -1.
 
 int num_active_clients = 0;
+
 client_t *clients[MAX_USERS];      // array of pointers to structs representing active clients.
 
- int *connfd;
-  socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
-  char client_hostname[MAXLINE], client_port[MAXLINE];
-  pthread_t tid;
+int *connfd;
+
+socklen_t clientlen;
+
+struct sockaddr_storage clientaddr;
+
+char client_hostname[MAXLINE], client_port[MAXLINE];
+
+pthread_t tid;
 
 client_t *cl;
-//TODO: accessing glocal variale
-/*
- * TODO: for implementing name_server.c, you will not receive as many
- * TODO: hints as you did for peer.c :) the two TODO's within the main()
- * TODO: function should therefore NOT be considered exhaustible;
- * TODO: you will need to figure out the rest on your own.
- */
 
 void echo(int con) {
+  
     size_t n;
     char buf[MAX_LINE];
     rio_t rio;
@@ -36,43 +36,34 @@ void echo(int con) {
     status = NONE;
     int IPPORT_READ = 0;
     int USERNAME_READ = 0;
-
     int logged_in = 0;
-
+    
     Rio_readinitb(&rio, con);
-   
+    
     while ((n=Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-
-        printf("%s\n", buf);
-
+        
         if ((strncmp("LOGIN", buf, MAX_REQUEST) == 0) && (status==NONE)) {
-           status = LOGIN_S;
-           
+           status = LOGIN_S;          
           continue;
         }
-                // else if (strncmp("LOOKUP", buf, 6) == 0 && (status != LOGIN_S))
-
-        
-        else if (strncmp("LOOKUP", buf, 6) == 0 && ((status == LOGIN_S_END) || (status == LOOKUP_S) || (status == LOOKUP_S_END)))
-        {
-
-          printf("%s\n", "YOLO");
+        else if (strncmp("LOOKUP", buf, 6) == 0 && ((status == LOGIN_S_END) 
+        || (status == LOOKUP_S) || (status == LOOKUP_S_END))) {
           status = LOOKUP_S;
           continue;
         }
-         else if ((strncmp("LOGOUT", buf, 6) == 0) && ((status == LOOKUP_S_END) || (status == LOGIN_S_END)))
-        {
+        else if ((strncmp("LOGOUT", buf, 6) == 0) && ((status == LOOKUP_S_END) 
+        || (status == LOGIN_S_END))) {
           printf("%s\n", "LOGGING OUT :)");
           status = LOGOUT_S;
           continue;
         }
 
         pthread_mutex_lock(&clients_mutex);
-
+        
         switch (status) {
-        case LOGIN_S:
 
-           if (USERNAME_READ == 0) {
+          case LOGIN_S:
+          if (USERNAME_READ == 0) {
             int i=0;
             while (buf[i] != '\0') {
               i++;
@@ -80,48 +71,14 @@ void echo(int con) {
             printf("yellow%s", buf);
               for (int j = 0; j < MAX_USERS; j++){
                 if (strncmp(buf, clients[j]->username, i-1) ==0) {
-                  
-                  // Rio_writen(con, ">>Logged in successfully!\n",28);
-
-                  printf("%s", "this works");
-
                   userID = j;
                   USERNAME_READ = 1;
-                  // clients[userID]->logged_in = 1;
-
                   break;
                 }
-                
-                
-                  
-                  // break;
-                
-                  
-              //TODO check else case if string is not matched
               }
-              if (USERNAME_READ==0)
-              {
+              if (USERNAME_READ==0) {
                   Rio_writen(con, ">>Login error!\n",28);             
               }
-              
-              
-          //   for (int j = 0; j < MAX_USERS; j++)
-          //   {
-          //       if (strncmp(buf, clients[j]->usernameAndPassword, i-1) ==0) {
-                  
-          //     Rio_writen(con, ">>Logged in successfully!\n",28);
-              
-          //     userID = j;
-
-          //     clients[userID]->logged_in = 1;
-
-          //     break;
-          //   }  
-          // }
-
-          
-          
-         
           break;
           }
 
@@ -131,269 +88,155 @@ void echo(int con) {
               i++;
             }
 
-            
-
-            if (strncmp(buf, clients[userID]->password, i-1) ==0) {
+          if (strncmp(buf, clients[userID]->password, i-1) ==0) {
               printf("%s\n", "works");    
-                  Rio_writen(con, ">>Logged in successfully!\n",26);
-            
+                  Rio_writen(con, ">>Logged in successfully!\n",26);          
                   clients[userID]->logged_in = 1;
                   IPPORT_READ = 1;
-
                   break;
-            } else
-            {
+            } 
+            else {
                USERNAME_READ=0;
                status = NONE;
                Rio_writen(con, ">>Login error!\n",15);
             }
-            
-            //TODO: check else case if string is not matched
-            
             break;
           }
-          
 
           if (n==9 && (IPPORT_READ==1)) {
-            // cl->port=buf;
             clients[userID]->port = strdup(buf); 
             printf("port is: %s\n", buf);
             break;
           }
+
           if (n==17 && (IPPORT_READ==1)) {
-            // cl->ip=buf;
             clients[userID]->ip = strdup(buf);
             printf("ip is: %s\n", buf);
-            
-            // chane enum here since login competed
             status = LOGIN_S_END;
-
             break;
           }
-
-//if using strncmp then count actual bytes.
-//if using n to compare then use actual bytes + 1 because of terminating null
-
           break;   
 
         case LOOKUP_S:
-        // buf != NULL
           printf("%s\n", "we are in");
           if (strlen(buf) > 0)
           {
-
             int found=0;
-            
-
             int i=0;
             while (buf[i] != '\0') {
               i++;
             }
-
-      
-
-
             for (int j = 0; j < MAX_USERS; j++){
-
-              // only check for logged in users
-              if (clients[j]->logged_in == 1)
-              {
+              if (clients[j]->logged_in == 1) {
                 if (strncmp(buf, clients[j]->username, i-1) ==0) {
                   printf("heh%s\n", clients[j]->ip);
-                    // char* t = strdup(clients[j]->username);
                     found=1;
                     Rio_writen(con, "1", strlen("1"));
                     Rio_writen(con, "\n" ,1);
-
                     Rio_writen(con, ">>UserName:" ,strlen(">>UserName:"));
                     Rio_writen(con, clients[j]->username ,strlen(clients[j]->username));
                     Rio_writen(con, "  ¦  " ,strlen("  ¦  "));
-
                     Rio_writen(con, ">>IP:" ,strlen(">>IP:"));
                     Rio_writen(con, clients[j]->ip ,strlen(clients[j]->ip));
                     Rio_writen(con, "  ¦  " ,strlen("  ¦  "));
-
                     Rio_writen(con, ">>Port:" ,strlen(">>Port:"));
                     Rio_writen(con, clients[j]->port ,strlen(clients[j]->port));
                     Rio_writen(con, "\n" ,1);
-
-
                   break;
-                }  
-              }
-              
-
-                
-              //TODO check else case if string is not matched
+                  }  
+                }
               }
 
-              if (found==0)
-              {
+              if (found==0) {
                 Rio_writen(con, ">>No user found" ,strlen(">>No user found"));
                 Rio_writen(con, "\n" ,1);
               }
-              
+            } 
 
-            } else{
-//TODO mutex in threading
-
-            
-            // int i=0;
-            // while (buf[i] != '\0') {
-            //   i++;
-            // }
-
-            
-
+            else {
             int count=0;
-
             for (int j = 0; j < MAX_USERS; j++){
-
               // only check for logged in users
-              if (clients[j]->logged_in == 1)
-              {
+              if (clients[j]->logged_in == 1) {
                 //number of logged in users
                 count++;
-
               }
-
-              //TODO check else case if string is not matched
             }
-
             printf("Amount of users: %d\n", count);
-
             char* cccc = malloc(sizeof(char));
             sprintf(cccc, "%d", count);
             Rio_writen(con, cccc , strlen(cccc));
             Rio_writen(con, "\n" , 1);
-
-             for (int j = 0; j < MAX_USERS; j++){
-
-              // only check for logged in users
-              if (clients[j]->logged_in == 1)
-              {
-
-                    Rio_writen(con, ">>UserName:" ,strlen(">>UserName:"));
-                    Rio_writen(con, clients[j]->username ,strlen(clients[j]->username));
-                    Rio_writen(con, "  ¦  " ,strlen("  ¦  "));
-
-                    Rio_writen(con, ">>IP:" ,strlen(">>IP:"));
-                    Rio_writen(con, clients[j]->ip ,strlen(clients[j]->ip));
-                    Rio_writen(con, "  ¦  " ,strlen("  ¦  "));
-
-                    Rio_writen(con, ">>Port:" ,strlen(">>Port:"));
-                    Rio_writen(con, clients[j]->port ,strlen(clients[j]->port));
-                    Rio_writen(con, "\n" ,1);
-
+            for (int j = 0; j < MAX_USERS; j++) {
+              if (clients[j]->logged_in == 1) {
+                Rio_writen(con, ">>UserName:" ,strlen(">>UserName:"));
+                Rio_writen(con, clients[j]->username ,strlen(clients[j]->username));
+                Rio_writen(con, "  ¦  " ,strlen("  ¦  "));
+                Rio_writen(con, ">>IP:" ,strlen(">>IP:"));
+                Rio_writen(con, clients[j]->ip ,strlen(clients[j]->ip));
+                Rio_writen(con, "  ¦  " ,strlen("  ¦  "));
+                Rio_writen(con, ">>Port:" ,strlen(">>Port:"));
+                Rio_writen(con, clients[j]->port ,strlen(clients[j]->port));
+                Rio_writen(con, "\n" ,1);
               }
-              
+            }            
+          }
 
-              //TODO check else case if string is not matched
-              }            
-
- 
-
-            }
-
-            status = LOOKUP_S_END;
-          
+          status = LOOKUP_S_END;        
           break;
 
-        case LOGOUT_S:
-
-        if (1)
-        {
-          /* code */  
-            
-          printf("jdfgjkdfbjkd%s\n", buf);
-
+          case LOGOUT_S:
+          if (1) {
             int i=0;
             while (buf[i] != '\0') {
               i++;
             }
-
-          for (int j = 0; j < MAX_USERS; j++){
-
+          for (int j = 0; j < MAX_USERS; j++) {
               // only check for logged in users
-            if (strncmp(buf, clients[j]->username, i-1) ==0){
-                 
-                    clients[j]->logged_in = 0;
-                    clients[j]->port = 0;
-                    clients[j]->ip = 0;
-                    Rio_writen(con, ">>Logged out" ,strlen(">>Logged out"));
-                    Rio_writen(con, "\n" ,1);
-
+            if (strncmp(buf, clients[j]->username, i-1) ==0){                 
+              clients[j]->logged_in = 0;
+              clients[j]->port = 0;
+              clients[j]->ip = 0;
+              Rio_writen(con, ">>Logged out" ,strlen(">>Logged out"));
+              Rio_writen(con, "\n" ,1);
             }
-              
-
-              //TODO check else case if string is not matched
           }            
-
-        // how to decide which struct to erase?
-
           break;  
-
         }
-
-        default:
-          break;
-        }
-
-        pthread_mutex_unlock(&clients_mutex);
-
-    }  
+        default: break;
+      }
+      
+    pthread_mutex_unlock(&clients_mutex);
+  }  
 }
 
 void DBSETUP(){
-
-  for (int i = 0; i < MAX_USERS; i++)
-  {
+  for (int i = 0; i < MAX_USERS; i++) {
     clients[i] = malloc(sizeof(client_t));
-
     clients[i]->logged_in = 0;
   }
-
   clients[0]->username = "a";
   clients[1]->username = "b";
   clients[2]->username = "c";
-  clients[3]->username = "sara";
-  clients[4]->username = "nikolai";
+  clients[3]->username = "LOGIN";
+  clients[4]->username = "LOOKUP";
 
   clients[0]->password = "a";
   clients[1]->password = "b";
   clients[2]->password = "c";
-  clients[3]->password = "JokerIsUnderrated";
-  clients[4]->password = "ColaHeist";
-
-  
-
-  // clients[5]->usernameAndPassword = "bb";
-  // clients[6]->usernameAndPassword = "ss";
-  // clients[7]->usernameAndPassword = "gg";
-  // clients[8]->usernameAndPassword = "lollol";
-  // clients[9]->usernameAndPassword = "troglador";
-  // clients[10]->usernameAndPassword = "nnn";
-  // clients[11]->usernameAndPassword = "flak";
-  // clients[12]->usernameAndPassword = "hahah";
-  // clients[13]->usernameAndPassword = "hehe";
-  // clients[14]->usernameAndPassword = "nene";
-  // clients[15]->usernameAndPassword = "kk";
-
+  clients[3]->password = "LOGIN";
+  clients[4]->password = "LOOKUP";
 }
 
 void *thread(void *vargp){
   int confd = *((int *)vargp);
   Pthread_detach(pthread_self());
   Free(vargp);
-
   echo(confd);
-
   Close(confd);
   return NULL;
 }
-
 int main(int argc, char **argv) { 
-
   if (argc != MAIN_ARGNUM + 1) {
     fprintf(stderr, "Usage: %s <listening port>\n", argv[0]);
     exit(EXIT_FAILURE);
@@ -401,57 +244,21 @@ int main(int argc, char **argv) {
     fprintf(stderr, ">> Invalid port number: %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
-
-  
-
   snprintf(listen_port, PORT_LEN, argv[1]);
-
   for (int i = 0; i < MAX_USERS; i++)
     clients[i] = NULL;
-
   DBSETUP();
-
   printf(">> Accepting peers on port %s ...\n", listen_port);
-
-  /*
-   * TODO: SETUP LISTENING SOCKET HERE
-   * HINT: remember that you are free to use everything from csapp.c ;) ;)
-   */
   listen_socket = Open_listenfd(listen_port);
-
   int running = 1;
   while (running) {
     clientlen = sizeof(struct sockaddr_storage);
-  
-
     connfd = malloc(sizeof(int));
     *connfd = Accept(listen_socket, (SA*)&clientaddr, &clientlen);
-    
     Getnameinfo((SA*)&clientaddr, clientlen, client_hostname, MAXLINE,
-          client_port, MAXLINE, 0);
-
+      client_port, MAXLINE, 0);
     printf("Connected to (%s, %s)\n", client_hostname, client_port);
-
     Pthread_create(&tid, NULL, thread,connfd);
-
-
-
-    /*
-     * TODO: LISTEN FOR PEERS HERE
-     * HINT: accept new peers on the listen_socket set up earlier.
-     * HINT: you may want to represent clients with a struct. a template
-     * HINT: struct client_t is given in name_server.h, which you can extend.
-     * 
-     * HINT: consider why this is wrapped in a loop - once a
-     * HINT: client connects, the server should delegate servicing
-     * HINT: the client and continue listening for new clients :)
-     *
-     *
-     * HINT: read Beej's guide: http://beej.us/guide/bgnet/html/
-     * HINT: (in particular the "System Calls or Bust" section)
-     *
-     */
   }
-
   exit(EXIT_SUCCESS);
 }
--- A6/src/peer.c
+++ A6/src/peer.c
@@ -1,11 +1,14 @@
 #include "peer.h"
-
 char name_server_ip[IP_LEN];       // hostname and port of name server - these
+
 char name_server_port[PORT_LEN];   // are passed as command line arguments.
+
 int  name_server_socket = -1;      // socket to the name server. initialized to -1.
+
 char buf[MAXLINE];
 
 char my_ip[IP_LEN];                // my_ip and my_port are set on /login, and are used for listening.
+
 char my_port[PORT_LEN];
 
 char my_username[USERNAME_LEN];
@@ -14,111 +17,91 @@ char usernameAndPassword[MAXLINE];
 
 int logged_in = 0;
 
+
 int main(int argc, char **argv) {
 
   if (argc != MAIN_ARGNUM + 1) {
     fprintf(stderr, "Usage: %s <name server IP> <name server port>.\n", argv[0]);
     exit(EXIT_FAILURE);
+
   } else if (!is_valid_ip(argv[1])) {
+
     fprintf(stderr, ">> Invalid name server IP: %s\n", argv[1]);
     exit(EXIT_FAILURE);
+
   } else if (!is_valid_port(argv[2])) {
+
     fprintf(stderr, ">> Invalid name server port: %s\n", argv[2]);
     exit(EXIT_FAILURE);
-  }
 
+  }
   snprintf(name_server_ip,   IP_LEN,   argv[1]);
+
   snprintf(name_server_port, PORT_LEN, argv[2]);
 
   printf(">> Connecting to name server at %s:%s ...\n",
       name_server_ip, name_server_port);
-
-  /*
-   * TODO #1
-   * TODO: SETUP NAME SERVER CONNECTION HERE
-   * HINT: use the specified ip and port to setup a socket to the name server.
-   * HINT: remember that you are free to use everything from csapp.c.
-   */
-  // Open_clientfd
    name_server_socket = Open_clientfd(name_server_ip, name_server_port);
-  /*
-   * we use the RIO library from csapp.c to read user input line by line.
-   */
   rio_t rio;
   char rio_buf[MAX_LINE];
   ssize_t num_read;
+  
   Rio_readinitb(&rio, STDIN_FILENO);
-
+  
   command_t command;          // current command, and array of size MAX_USER_ARGNUM
+  
   args_t    args;             // holding arguments to current command (see peer.h).
-
+  
   char *username, *password;  // these pointers will serve different
+  
   char *ip, *port;            // purposes based on the current command.
+  
   char *message;
+  
   char *saveduser;
-
-  //TODO move this in worker thread later
+  
   char buf2[MAXLINE];
+  
   char buf3[MAXLINE];
+  
   char buf4[MAXLINE];
+  
   rio_t rio2;
+  
   rio_t rio3;
-
+  
   int running = 1;
-  while (running) {
+  
+  while (running) { 
 
-    /*
-     * read line of user input and parse
-     * the command, storing arguments in args.
-     */
     if ((num_read = Rio_readlineb(&rio, rio_buf, MAX_LINE)) < 0) {
       fprintf(stderr, "rio_read() error: %s\n", strerror(errno));
       exit(EXIT_FAILURE);
-    } else if (num_read <= 1) continue; // if input is an empty line or EOF.
-
-
+    }
+     else if (num_read <= 1) continue; // if input is an empty line or EOF.
+    
     command = parse_command(rio_buf, args); // see common.h for a description of parse_command()
-
+    
     switch (command) {
 
-      case LOGIN:
+      case LOGIN: 
 
-        
-        
         Rio_readinitb(&rio2, name_server_socket);
-
         if (logged_in) {
           printf(">> /login error: already logged in as %s\n", my_username);
           break;
         }
-
+        
         username = args[0];                // username and password to login with.
         password = args[1];
         ip       = args[2];                // ip and port that the name server should respond to
         port     = args[3];                // (eg. with messages from other users).
-
+        
         snprintf(my_ip,   IP_LEN,   ip);   // write ip and port to my_ip and my_port
         snprintf(my_port, PORT_LEN, port);
-
-        char LOGIN_REQUEST[] = "LOGIN"; 
-
-        /*
-         * TODO #2
-         * TODO: LOG INTO NAME SERVER HERE.
-         * HINT: use the established connection to the
-         * HINT: name server to send a login request
-         * HINT: write to the name_server_socket set up earlier; recall that a
-         * HINT: socket is functionally similar to a file descriptor, and
-         * HINT: is written to similarly. you can for example use the RIO library,
-         * HINT: but otherwise google is your friend.
-         *
-         * HINT: eventually, you want to set logged_in to 1, but depending
-         * HINT: on your protocol, you may want to somehow confirm the login first :)
-         */
-
         
-        // logged_in = 1; 
-            
+        char LOGIN_REQUEST[] = "LOGIN"; 
+        
         Rio_writen(name_server_socket, LOGIN_REQUEST, strlen(LOGIN_REQUEST));   
         Rio_writen(name_server_socket, "\n", 1);
         Rio_writen(name_server_socket, username, strlen(username));
@@ -129,242 +112,131 @@ int main(int argc, char **argv) {
         Rio_writen(name_server_socket, "\n", 1);
         Rio_writen(name_server_socket, my_ip, IP_LEN);
         Rio_writen(name_server_socket, "\n", 1);  
-
+        
         size_t n;
-
-
+        
         while (1) {
           n=Rio_readlineb(&rio2, buf2,MAXLINE);
           if (n>0) {
-            
-            TODO: // make some check to ensure log in cred. are correct
-
-            // printf("%s", buf);
-
-            // int i;
-            // i = 0;
-            // while (buf2[i] != '\0') {
-            //   i++;
-            // }
-
-
-            if (strncmp(">>Logged in successfully!", buf2, 25) == 0)
-            {
+            if (strncmp(">>Logged in successfully!", buf2, 25) == 0) {
               logged_in = 1;
             } 
-            
-
-
-            Fputs(buf2, stdout);
-            
+            Fputs(buf2, stdout);          
             // save username in variable
             saveduser = strdup(username);
-
             break;
           }
-        }
         
-
-        break;
-
+        }
+      break;
+      
       case LOOKUP:
+       
         if (!logged_in) {
           printf(">> /lookup error: not logged onto name server.\n");
           break;
         }
-
-        // Rio_readinitb(&rio3, name_server_socket);
-
+        
         char LOOKUP_REQUEST[] = "LOOKUP"; 
-
-        // username = malloc(sizeof(char));
-
-
         username = args[0]; // username to lookup (may be null)
-
         
-        
-
-        // printf("%ld", strlen((char*) username));
-
-        // if (username == NULL)
-        // {
-          
-        // }
-        
-
         Rio_writen(name_server_socket, LOOKUP_REQUEST, strlen(LOOKUP_REQUEST));   
         Rio_writen(name_server_socket, "\n", 1);
-        if (username != NULL)
-        {
-          Rio_writen(name_server_socket, username, strlen(username));
-          
-        } else {
+       
+        if (username != NULL) {
+          Rio_writen(name_server_socket, username, strlen(username));     
+        } 
+        else {
           Rio_writen(name_server_socket, "", 1);
         }
+       
         Rio_writen(name_server_socket, "\n", 1);
-
-
-        
-
-        // Rio_writen(name_server_socket, name, strlen(name));
-        // Rio_writen(name_server_socket, "\n", 1);  
-        
         int counter=0;
         int numOfUsers=0;
-
-        // while (1)
-        // {
-        //   n=Rio_readlineb(&rio2, buf3,MAXLINE);
-        //   Fputs(buf3, stdout);
-        // }
-        
-
+       
         while (1) {
           n=Rio_readlineb(&rio2, buf3,MAXLINE);
           if (n>0) {
-
-
-            if (strncmp(">>No user found", buf3, 15) == 0)
-            {
+            if (strncmp(">>No user found", buf3, 15) == 0) {
               Fputs(buf3, stdout);
               break;
             } 
-       
-
             if (counter == 0){
               numOfUsers=atoi(buf3);
               counter++;
               continue;
             }
-
-            //printf("%d\n", atoi(buf3));
-            
-            
             Fputs(buf3, stdout);
-
-            if (counter == numOfUsers)
-            {
+            if (counter == numOfUsers) {
               break;
             }
-
-            counter++;
-       
+            counter++;      
           }
         }
-        
-        /*
-         * TODO #3
-         * TODO: LOOKUP USERS HERE.
-         *
-         * HINT: recall that the API specifies that the user can
-         * HINT: optionally omit a username to the lookup command.
-         * HINT: you can choose to either handle it here or server-
-         * HINT: side, depending on your chosen protocol.
-         */
+       
         break;
-
-
+      
       case LOGOUT:
+        
         if (!logged_in) {
           printf(">> /logout error: not logged onto name server.\n");
           break;
         }
-
-
-        /*
-         * TODO #4
-         * TODO: LOGOUT OF NAME SERVER HERE.
-         *
-         * HINT: as with /login, you eventually want to set logged_in to 0.
-         */
-        // logged_in = 0;
-
-        // printf("%s\n", saveduser);
-             
+      
         Rio_writen(name_server_socket, "LOGOUT", strlen("LOGOUT"));   
         Rio_writen(name_server_socket, "\n", 1);
-
         Rio_writen(name_server_socket, saveduser, strlen(saveduser));
-        Rio_writen(name_server_socket, "\n", 1);
-      
+        Rio_writen(name_server_socket, "\n", 1);    
+       
         while (1) {
           n=Rio_readlineb(&rio2, buf3,MAXLINE);
-          if (n>0) {
-            
+          if (n>0) {          
             Fputs(buf3, stdout);
             logged_in=0;
             break;
           }
-        }
-        
+        }        
         break;
-
-
+     
       case EXIT:
-        
-        /*
-         * TODO #5
-         * TODO: EXIT CLIENT HERE.
-         *
-         * HINT: as is, the client simply exits. depending on your protocol,
-         * HINT: consider what should happen if the user is logged in at exit.
-         */
-
-        if (logged_in == 1)
-        {
+       
+        if (logged_in == 1) {
           printf("%s\n", "Please logout first");
-        } else {
+        } 
+        else {
           free(saveduser);
           running = 0;
         }
-        
-
         break;
-
-
+     
       case MSG:
-        /*
-         * NOT REQUIRED FOR A6. we save the actual messaging for A7.
-         */
-
         if (!logged_in) {
           printf(">> /msg error: not logged onto name server.\n");
           break;
         }
         username = args[1]; // username of recipient.
         message  = args[2]; // actual message to send.
+
+        
+
         break;
-
-
+     
       case SHOW:
-        /*
-         * NOT REQUIRED FOR A6. we leave the actual messaging for A7
-         */
-
         if (!logged_in) {
           printf(">> /show error: not logged onto name server.\n");
           break;
         }
         username = args[1]; // name of user to show messages from (may be NULL)
         break;
-
-
       case ERROR:
         printf(">> Error: unknown command or wrong number of arguments.\n");
         break;
+   
     }
-    // running=0;
+  
   }
-
+ 
   printf(">> Closing client ...\n");
-  /*
-   * TODO #6
-   * TODO: CLOSE CONNECTION (ie. socket to name server) HERE.
-   * HINT: at this point, the client is (should be) properly logged out of
-   * HINT: the name server, so this step should be easy :)
-   */
   Close(name_server_socket);
-
   exit(EXIT_SUCCESS);
 }
